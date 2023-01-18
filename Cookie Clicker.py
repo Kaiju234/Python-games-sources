57727a2f257d1e22c4c90b2c2675804ffe0f0c20
Kaiju234 <68970395+Kaiju234@users.noreply.github.com>
2023-01-18 14:24:15 -0600

    Add files via upload

---
+++ Cookie Clicker.py
@@ -0,0 +1,30 @@
+import turtle 
+
+wn = turtle.Screen()
+wn.title("Cookie Clicker by @rajashah0303")
+wn.bgcolor("black")
+
+wn.register_shape("cookie.gif")
+
+cookie = turtle.Turtle()
+cookie.shape("cookie.gif")
+cookie.speed(0)
+
+clicks = 0
+
+pen = turtle.Turtle
+pen.hideturtle
+pen.color("blue")
+pen.penup()
+pen.goto(0,400)
+pen.write(f"Clicks: {clicks}", align="center", font=("Courier New", 32, "normal"))
+
+def clicked(x, y):
+    global clicks
+    clicks += 1
+    pen.clear()
+    pen.write(f"Clicks: {clicks}", align="center", font=("Courier New", 32, "normal"))
+
+cookie.onclick(clicked)
+
+wn.mainloop()
